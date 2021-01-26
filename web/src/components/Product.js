4908546f361444e579e0c1e3b378e6064d4e3556
Last Whisper <xxzworkmail@gmail.com>
2021-01-26 18:55:11 +0800

    创建主页页面

---
+++ web/src/components/Product.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+
+// 产品组件
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 py=3 rounded">
+      <a href={`/products/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a href={`/products/${product._id}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </a>
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews}条评论`} />
+        </Card.Text>
+        <Card.Text as="h3">￥{product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
