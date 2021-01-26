db0e39b552ab59cff0ff2d3b5361ffbbe9ddb32c
Last Whisper <xxzworkmail@gmail.com>
2021-01-26 20:27:19 +0800

    完成产品详情页

--- web/src/screens/HomeScreen.js
+++ web/src/screens/HomeScreen.js
@@ -6,7 +6,6 @@ import Product from '../components/Product'
 const HomeScreen = () => {
   return (
     <div>
-      <h1></h1>
       <Row>
         {products.map(product => (
           <Col key={product._id} sm={12} md={6} lg={3} xl={3}>
