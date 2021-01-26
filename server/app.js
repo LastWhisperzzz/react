1d24cf75eb947a54b3af0c17c25a59589aca8c3a
Last Whisper <xxzworkmail@gmail.com>
2021-01-26 21:08:52 +0800

    后台项目初始化

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+
+const products = require('./data/products')
+
+const app = express()
+// 跨域
+app.use(cors())
+// 将请求转为json
+app.use(express.json())
+
+app.get('/api/products', (req, res) => {
+  res.send(products)
+})
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(product => product._id === req.params.id)
+  res.send(product)
+})
+
+app.listen(3000, () => {
+  console.log('App running at: http://localhost:3000')
+})
