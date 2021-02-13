ad4a8b5bff8c6b57ed5534aec7b334dfa4c0dfa4
Last Whisper <xxzworkmail@gmail.com>
2021-02-13 23:49:13 +0800

    产品详情reducers&action

--- web/src/pages/ProductPage.jsx
+++ web/src/pages/ProductPage.jsx
@@ -1,68 +1,72 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {listProductDetails} from '../redux/actions/productActions';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 // 产品详情页
 const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState({})
-
+  const dispatch = useDispatch()
+  const productDetails = useSelector(state=> state.productDetails)
+  const { loading, error, product } = productDetails
+  console.log(productDetails);
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`)
-      setProduct(data)
-    }
-    fetchProduct()
-  }, [match])
+   dispatch(listProductDetails(match.params.id))
+  }, [match,dispatch])
 
   return (
     <>
       <Link className="btn btn-dark my-3" to="/">
         返回主页
       </Link>
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews}条评论`} />
-            </ListGroup.Item>
-            <ListGroup.Item>价格:￥{product.price}</ListGroup.Item>
-            <ListGroup.Item>描述:{product.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <Row>
-                  <Col>价格:</Col>
-                  <Col>
-                    <strong>￥{product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>库存:</Col>
-                  <Col>{product.countInStock > 0 ? '有货' : '无货'}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
-                  添加到购物车
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+      {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
+        (
+           <Row>
+           <Col md={6}>
+             <Image src={product.image} alt={product.name} fluid />
+           </Col>
+           <Col md={3}>
+             <ListGroup variant="flush">
+               <ListGroup.Item>
+                 <h3>{product.name}</h3>
+               </ListGroup.Item>
+               <ListGroup.Item>
+                 <Rating value={product.rating} text={`${product.numReviews}条评论`} />
+               </ListGroup.Item>
+               <ListGroup.Item>价格:￥{product.price}</ListGroup.Item>
+               <ListGroup.Item>描述:{product.description}</ListGroup.Item>
+             </ListGroup>
+           </Col>
+           <Col md={3}>
+             <Card>
+               <ListGroup variant="flush">
+                 <ListGroup.Item>
+                   <Row>
+                     <Col>价格:</Col>
+                     <Col>
+                       <strong>￥{product.price}</strong>
+                     </Col>
+                   </Row>
+                 </ListGroup.Item>
+                 <ListGroup.Item>
+                   <Row>
+                     <Col>库存:</Col>
+                     <Col>{product.countInStock > 0 ? '有货' : '无货'}</Col>
+                   </Row>
+                 </ListGroup.Item>
+                 <ListGroup.Item>
+                   <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                     添加到购物车
+                   </Button>
+                 </ListGroup.Item>
+               </ListGroup>
+             </Card>
+           </Col>
+         </Row>
+      )}
     </>
   )
 }
--- web/src/redux/store.js
+++ web/src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 
 // 汇总reducer
-const reducer = combineReducers({ productList: productListReducer })
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer
+})
 
 const initialState = {}
 
