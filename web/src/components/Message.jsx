b9780d9b6945510a951f738770643f182cbdae92
Last Whisper <xxzworkmail@gmail.com>
2021-02-14 23:20:39 +0800

    完成购物车页面

--- web/src/components/Message.jsx
+++ web/src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Alert } from 'react-bootstrap'
 
 // 消息组件
-const Message = () => {
+const Message = ({ variant, children }) => {
   return (
     <Alert variant={variant}>{children}</Alert>
   )
