0e2b6bd5a4c684459e5268aed42dfad4aa3c5975
Last Whisper <xxzworkmail@gmail.com>
2021-02-16 23:28:29 +0800

    完成用户登录页面

--- web/src/redux/store.js
+++ web/src/redux/store.js
@@ -3,11 +3,14 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
+
 // 汇总reducer
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer
 })
 
 //获取本地存储的购物车信息
@@ -15,11 +18,15 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+//获取本地存储的登录用户信息
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 //初始化state值
 const initialState = {
-  cart: {
-    cartItems: cartItemsFromStorage
-  }
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage }
 }
 
 // redux-thunk，用于支持异步action
