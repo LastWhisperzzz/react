4efb62d17f91e1497e5e67b2e7577fd3d55cfb8b
Last Whisper <xxzworkmail@gmail.com>
2021-02-17 20:22:51 +0800

    收货地址页面

--- web/src/redux/actions/cartActions.js
+++ web/src/redux/actions/cartActions.js
@@ -33,3 +33,9 @@ export const removeFromCart = id => async (dispatch, getState) => {
   })
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
+
+// 保存收货地址action
+export const saveShippingAddress = data => async dispatch => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data })
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
+}
