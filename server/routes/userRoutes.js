a04dc25fba2ca3234979ab483f49cb4cbce1e398
Last Whisper <xxzworkmail@gmail.com>
2021-02-18 21:23:01 +0800

    后台管理:用户列表修改接口

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,12 +5,21 @@ const {
   authUser,
   getUserProfile,
   updateUserProfile,
-  getUsers
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser
 } = require('../controllers/userController')
 const { protect, admin } = require('../middleware/authMiddleware')
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+// 后台管理:用户列表增删改查
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 module.exports = router
