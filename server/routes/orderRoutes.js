6fff137600c1e31189e67b3792ea01e3f5d14106
Last Whisper <xxzworkmail@gmail.com>
2021-02-18 18:00:51 +0800

    获取订单详情接口

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const addOrderItems = require('../controllers/orderContorller')
+const { addOrderItems, getOrderById } = require('../controllers/orderContorller')
 const protect = require('../middleware/authMiddleware')
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
+
+module.exports = router
